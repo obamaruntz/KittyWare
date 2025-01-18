1826e2c83c2df50539b1aca8b7037a560b4b7d5d
ObamaRuntz <150731631+obamaruntz@users.noreply.github.com>
2025-01-18 17:33:48 +0000

    bunch of progress on menu design

--- KittyWare/src/meow.cpp
+++ KittyWare/src/meow.cpp
@@ -27,62 +27,61 @@ auto main() -> int {
 	GetOverlay();
 	MapDriver();
     
-    //Console::Log<Console::PrintType::LOADING>("grcwindow not found. Please open FiveM...\n");
-    //while (Cache::hwnd == nullptr) {
-    //    Cache::hwnd = FindWindowA("grcwindow", nullptr);
-    //    if (Cache::hwnd != nullptr) break;
-    //    else Sleep(100);
-    //}
+    Console::Log<Console::PrintType::LOADING>("grcwindow not found. Please open FiveM...\n");
+    while (Cache::hwnd == nullptr) {
+        Cache::hwnd = FindWindowA("grcwindow", nullptr);
+        if (Cache::hwnd != nullptr) break;
+        else Sleep(100);
+    }
 
-    //std::string process_name = Gate::GetProcessNameViaHWND(Cache::hwnd);
-    //Console::Log<Console::PrintType::DEBUG>("Raw Process Name: " + process_name + "\n");
-    //size_t pos = process_name.find_last_of("\\");
-    //process_name = process_name.substr(pos + 1);
+    std::string process_name = Gate::GetProcessNameViaHWND(Cache::hwnd);
+    Console::Log<Console::PrintType::DEBUG>("Raw Process Name: " + process_name + "\n");
+    size_t pos = process_name.find_last_of("\\");
+    process_name = process_name.substr(pos + 1);
 
-    //Cache::game_build = Gate::ExtractNumber(process_name);
+    Cache::game_build = Gate::ExtractNumber(process_name);
 
-    //// default game build
-    //if (process_name == "FiveM_GameProcess.exe" || process_name == "FiveM_GTAProcess.exe")
-    //    Cache::game_build = 1604;
+    // default game build
+    if (process_name == "FiveM_GameProcess.exe" || process_name == "FiveM_GTAProcess.exe")
+        Cache::game_build = 1604;
 
-    //DWORD process_id;
-    //GetWindowThreadProcessId(Cache::hwnd, &process_id);
-    //Console::Log<Console::PrintType::DEBUG>("HWND Process ID:" + std::to_string(process_id) + "\n");
+    DWORD process_id;
+    GetWindowThreadProcessId(Cache::hwnd, &process_id);
+    Console::Log<Console::PrintType::DEBUG>("HWND Process ID: " + std::to_string(process_id) + "\n");
 
-    //Gate::process_id = process_id;
-    //Cache::base = Gate::GetModuleBaseAddress(Gate::process_id, process_name.c_str());
-    //Console::Log<Console::PrintType::DEBUG>("FiveM Image Base: " + std::to_string(Cache::base) + "\n");
+    Gate::process_id = process_id;
+    Cache::base = Gate::GetModuleBaseAddress(Gate::process_id, process_name.c_str());
+    Console::Log<Console::PrintType::DEBUG>("FiveM Image Base: " + std::to_string(Cache::base) + "\n");
 
-    //Offsets::Set(Cache::game_build);
-    //if (Gate::process_id != 0 && Cache::base != 0 && Cache::hwnd != 0)
-    //    Console::Log<Console::PrintType::OK>("FiveM found successfully - b" + std::to_string(Cache::game_build) + "\n");
-    //else {
-    //    Console::Log<Console::PrintType::FAIL>("0xDEAD Something went severly wrong. Refer to the wiki. The process was either killed or an anticheat/antivirus is interfering. If this is a recurring problem, contact support via discord asap. The software will close in 10 seconds.\n");
-    //    Sleep(10000);
-    //    exit(0);
-    //    return 0;
-    //}
-
-    Gate::handle = CreateFile("\\\\.\\Krypted", GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-    if (Gate::handle == INVALID_HANDLE_VALUE) {
-        Console::Log<Console::PrintType::FAIL>("0xEAC Run Krypted as Administrator. Refer to the wiki. If it is already running as Administrator, check if FACEIT anticheat is running, if so right click and disable it. Or if vanguard is running, stop that. This error usually happens due to anticheats. If the problem persisnts, contact us on discord. The software will close in 10 seconds.\n");
+    Offsets::Set(Cache::game_build);
+    if (Gate::process_id != 0 && Cache::base != 0 && Cache::hwnd != 0)
+        Console::Log<Console::PrintType::OK>("FiveM found successfully - b" + std::to_string(Cache::game_build) + "\n");
+    else {
+        Console::Log<Console::PrintType::FAIL>("0xDEAD Something went severly wrong. Refer to the wiki. The process was either killed or an anticheat/antivirus is interfering. If this is a recurring problem, contact support via discord asap. The software will close in 10 seconds.\n");
         Sleep(10000);
         exit(0);
         return 0;
     }
 
-    //if (!Driver::Attach(Gate::handle, Gate::process_id))
-    //{
-    //    Console::Log<Console::PrintType::FAIL>("0xEAF Something went wrong. The driver failed to attach to the process. Please refer to wiki. If the problem persisnts, contact us on discord. The software will close in 10 seconds.\n");
-    //    Sleep(10000);
-    //    exit(0);
-    //    return 0;
-    //}
+    Gate::handle = CreateFile("\\\\.\\Krypted", GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+    if (Gate::handle == INVALID_HANDLE_VALUE) {
+        Console::Log<Console::PrintType::FAIL>("0xEAC Run KittyWare as Administrator. Refer to the wiki. If it is already running as Administrator, check if FACEIT anticheat is running, if so right click and disable it. Or if vanguard is running, stop that. This error usually happens due to anticheats. If the problem persisnts, contact us on discord. The software will close in 10 seconds.\n");
+        Sleep(10000);
+        exit(0);
+        return 0;
+    }
+
+    if (!Driver::Attach(Gate::handle, Gate::process_id)) {
+        Console::Log<Console::PrintType::FAIL>("0xEAF Something went wrong. The driver failed to attach to the process. Please refer to wiki. If the problem persisnts, contact us on discord. The software will close in 10 seconds.\n");
+        Sleep(10000);
+        exit(0);
+        return 0;
+    }
 
     std::thread(Render).detach();
 
     while (true) {
-        Sleep(10000);
+        Sleep(10);
     }
 
     return 0;
@@ -111,12 +110,16 @@ void GetOverlay()
         return;
     }
 
+
+    Cache::menu_hwnd = overlay_hwnd;
     Console::Log<Console::PrintType::OK>("Medal Overlay has been extended into client area via DWM successfully!\n");
 
     SetWindowPos(overlay_hwnd, NULL, 0, 0, 1920, 1080, SWP_HIDEWINDOW);
     UpdateWindow(overlay_hwnd);
-
-    Cache::menu_hwnd = overlay_hwnd;
+    SetWindowLong(overlay_hwnd, GWL_EXSTYLE, (int)GetWindowLong(overlay_hwnd, GWL_EXSTYLE) | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+    SetLayeredWindowAttributes(overlay_hwnd, 0, 255, LWA_ALPHA);
+    SetLayeredWindowAttributes(overlay_hwnd, RGB(0, 0, 0), 0, ULW_COLORKEY);
+    UpdateWindow(overlay_hwnd);
 }
 
 bool GhostCallback(ULONG64* param1, ULONG64* param2, ULONG64 allocationPtr, ULONG64 allocationSize, ULONG64 mdlptr) {
--- KittyWare/src/render/render.h
+++ KittyWare/src/render/render.h
@@ -8,6 +8,9 @@
 #include <raw/logo.h>
 #include <raw/mont.h>
 #include <d3dx9tex.h>
+#include <raw/icons.h>
+#include <imgui/imgui_internal.h>
+#include <raw/hashes.h>
 
 HWND hwnd;
 ImGuiIO* hook_io = nullptr;
@@ -21,20 +24,27 @@ void CleanupDeviceD3D();
 void ResetDevice();
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
+ImVec2 menu_pos;
+ImVec2 menu_size;
+void NavbarMenu();
+
+ImFont* fa;
+
 ImVec4 fix(int w, int x, int y, int z) {
     return ImVec4(w * (1.0f / 255.0f), x * (1.0f / 255.0f), y * (1.0f / 255.0f), z * (1.0f / 255.0f));
 }
 
 enum heads {
     HEAD_LOCAL,
-    HEAD_VISUAL,
     HEAD_AIM,
-    HEAD_TELEPORT,
+    HEAD_VISUAL,
     HEAD_WORLD,
     HEAD_MISCELLANEOUS,
     HEAD_SETTINGS,
     HEAD_EXIT
 };
+
+static heads tab = HEAD_LOCAL;
 
 auto* Logo_Texture = LPDIRECT3DTEXTURE9();
 
@@ -59,21 +69,27 @@ inline int Render() {
     CreateContext();
     ImGuiIO& io = GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   
-    io.IniFilename = nullptr;
-    io.LogFilename = nullptr;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.IniFilename = nullptr;    io.LogFilename = nullptr;
 
-    io.Fonts->AddFontFromMemoryTTF(Mont_Font, sizeof Mont_Font, 20.0f);
+    io.Fonts->AddFontFromMemoryTTF(Sans, sizeof Sans, 22.0f);
+
+    static const ImWchar icon_ranges[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
+    ImFontConfig icons_config;
+    icons_config.MergeMode = true;
+    icons_config.PixelSnapH = true;
+    icons_config.OversampleH = 3;
+    icons_config.OversampleV = 3;
+    fa = io.Fonts->AddFontFromMemoryTTF(&font_awesome, sizeof font_awesome, 26.f, &icons_config, icon_ranges);
 
     hook_io = &GetIO();
 
+    StyleColorsDark();
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX9_Init(g_pd3dDevice);
 
-    StyleColorsDark();
-
     ImGuiStyle& style = GetStyle();
-    style.Colors[ImGuiCol_WindowBg] = fix(15, 15, 15, 255);
+    style.Colors[ImGuiCol_WindowBg] = fix(35, 35, 35, 255);
 
     style.Colors[ImGuiCol_Button] = fix(23, 23, 23, 255);
     style.Colors[ImGuiCol_ButtonHovered] = fix(28, 28, 28, 255);
@@ -90,32 +106,27 @@ inline int Render() {
     style.Colors[ImGuiCol_PopupBg] = fix(17, 17, 17, 255);
     style.Colors[ImGuiCol_Border] = fix(37, 37, 37, 255);
 
-    style.TabRounding = 50.f;
-    style.FrameRounding = 50.f;
-    style.GrabRounding = 50.f;
-    style.WindowRounding = 50.f;
-    style.PopupRounding = 50.f;
+    style.TabRounding = 20.f;
+    style.FrameRounding = 20.f;
+    style.GrabRounding = 20.f;
+    style.WindowRounding = 20.f;
+    style.PopupRounding = 20.f;
 
     ImVec4 clear_color = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
 
     D3DXCreateTextureFromFileInMemory(g_pd3dDevice, Logo, sizeof Logo, &Logo_Texture);
 
-    static heads tab = HEAD_LOCAL;
-
-    bool z = false;
-    while (!z) {
-
-        if (GetAsyncKeyState(VK_END) & 1 || GetAsyncKeyState(VK_INSERT) & 1) {
+    bool done = false;
+    while (!done) {
+        if (GetAsyncKeyState(VK_INSERT) & 1) {
             long lWinLong = GetWindowLong(hwnd, GWL_EXSTYLE);
-            if (!is_menu_visible)
-            {
+            if (!is_menu_visible) {
                 is_menu_visible = !is_menu_visible;
                 if (lWinLong != (WS_EX_LAYERED | WS_EX_TOOLWINDOW))
                     SetWindowLong(hwnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TOOLWINDOW);
                 SetForegroundWindow(hwnd);
             }
-            else
-            {
+            else {
                 is_menu_visible = !is_menu_visible;
 
                 if (lWinLong != (WS_EX_LAYERED | WS_EX_TOOLWINDOW | WS_EX_TRANSPARENT))
@@ -123,7 +134,8 @@ inline int Render() {
             }
         }
 
-        if (z) break;
+        if (done)
+            break;
 
         ImGui_ImplDX9_NewFrame();
         ImGui_ImplWin32_NewFrame();
@@ -131,8 +143,7 @@ inline int Render() {
 
         POINT p;
 
-        if (GetCursorPos(&p))
-        {
+        if (GetCursorPos(&p)) {
             io.MousePos.x = p.x;
             io.MousePos.y = p.y;
         }
@@ -146,55 +157,64 @@ inline int Render() {
         static bool first_show = false;
         if (is_menu_visible) {
             if (!first_show) {
+                ShowWindow(::GetConsoleWindow(), SW_HIDE);
                 ShowWindow(hwnd, SW_SHOW);
                 first_show = true;
             }
-
-            HCURSOR hCursor = LoadCursor(NULL, IDC_ARROW);
+            HCURSOR hCursor = LoadCursor(NULL, IDC_CROSS);
             SetCursor(hCursor);
 
-            ImGui::SetNextWindowPos({ 0, 0 });
-            ImGui::SetNextWindowSize({ 800, 800 });
+            PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(10, 10));
+
+            SetNextWindowSize(ImVec2(1000, 500));
             Begin(
                 "KittyWare",
-                &z,
+                NULL,
                 ImGuiWindowFlags_NoTitleBar |
                 ImGuiWindowFlags_NoScrollbar |
                 ImGuiWindowFlags_NoCollapse |
                 ImGuiWindowFlags_NoResize
             );
 
-            //auto window = ImGui::GetCurrentWindow();
-      /*      auto draw = window->DrawList;
-            auto pos = window->Pos;
-            auto size = window->Size;*/
+            menu_pos = GetWindowPos();
+            menu_size = GetWindowSize();
 
-            BeginGroup();
-            {
-
-            }
-            EndGroup();
-
-            switch(tab) {
+            NavbarMenu();
+            
+            switch (tab) {
             case HEAD_LOCAL:
-                break;
-            case HEAD_VISUAL:
-                break;
-            case HEAD_AIM:
-                break;
-            case HEAD_TELEPORT:
-                break;
-            case HEAD_WORLD:
-                break;
-            case HEAD_MISCELLANEOUS:
-                break;
-            case HEAD_SETTINGS:
-                break;
-            case HEAD_EXIT:
-                break;
-            }
+                ImGui::BeginChild("##local", ImVec2(ImGui::GetWindowWidth() - 19, ImGui::GetWindowHeight() - 55));
 
+                float childHeight = ImGui::GetContentRegionAvail().y / 3.0f;
+
+                ImGui::BeginChild("Self", ImVec2(0, childHeight));
+                ImGui::Text("Self Content Here");
+                ImGui::EndChild();
+
+                ImGui::Spacing();
+
+                ImGui::BeginChild("Weapon", ImVec2(0, childHeight));
+                ImGui::Text("Weapon Content Here");
+                ImGui::EndChild();
+
+                ImGui::Spacing();
+
+                ImGui::BeginChild("Vehicle", ImVec2(0, childHeight));
+                ImGui::Text("Vehicle Content Here");
+                ImGui::EndChild();
+
+                ImGui::EndChild();
+            case HEAD_AIM:
+            case HEAD_VISUAL:
+            case HEAD_WORLD:
+            case HEAD_MISCELLANEOUS:
+            case HEAD_EXIT:
+                printf("ew");
+            }
+            
             End();
+
+            PopStyleVar();
         }
 
         EndFrame();
@@ -203,12 +223,12 @@ inline int Render() {
         g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
         D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x * clear_color.w * 255.0f), (int)(clear_color.y * clear_color.w * 255.0f), (int)(clear_color.z * clear_color.w * 255.0f), (int)(clear_color.w * 255.0f));
         g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
-        if (g_pd3dDevice->BeginScene() >= 0)
-        {
+        if (g_pd3dDevice->BeginScene() >= 0) {
             ImGui::Render();
             ImGui_ImplDX9_RenderDrawData(GetDrawData());
             g_pd3dDevice->EndScene();
         }
+
 
         HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 
@@ -219,9 +239,70 @@ inline int Render() {
     ImGui_ImplDX9_Shutdown();
     ImGui_ImplWin32_Shutdown();
     DestroyContext();
+
+    //UnhookWindowsHookEx(hMouseHook);
+
     CleanupDeviceD3D();
     DestroyWindow(hwnd);
+
     return 0;
+}
+inline void NavbarMenu() {
+    using namespace ImGui;
+    PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(10, 10));
+    PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2(0, 0));
+    PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+    PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0, 0, 0, 0));
+    PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0, 0, 0, 0));
+
+    SetNextWindowPos(ImVec2(menu_pos.x, menu_pos.y - 80), ImGuiCond_Always);
+    Begin(
+        "NavbarMenu",
+        nullptr,
+        ImGuiWindowFlags_NoDecoration
+    );
+    SetWindowSize({ 1000, 70 });
+
+    Image(reinterpret_cast<ImTextureID>(Logo_Texture), ImVec2(100, 50));
+
+    float windowHeight = GetWindowHeight();
+    float buttonHeight = 50;
+    float verticalOffset = (windowHeight - buttonHeight) * 0.5f;
+
+    SameLine();
+    SetCursorPosX(GetWindowWidth() - 410);
+    SetCursorPosY(verticalOffset);
+
+    ImGui::PushFont(fa);
+
+    auto RenderTabButton = [&](heads tabType, const char* icon, const char* label) {
+        if (tab == tabType) {
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+        }
+        else {
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.5f, 0.5f, 0.5f, 1.0f));
+        }
+
+        if (ImGui::Button(icon, ImVec2(50, buttonHeight))) {
+            tab = tabType;
+        }
+        ImGui::PopStyleColor();
+        ImGui::SameLine();
+    };
+
+    RenderTabButton(HEAD_LOCAL, ICON_FA_USER, "Local");
+    RenderTabButton(HEAD_AIM, ICON_FA_CROSSHAIRS, "Aim");
+    RenderTabButton(HEAD_VISUAL, ICON_FA_EYE, "Visual");
+    RenderTabButton(HEAD_WORLD, ICON_FA_GLOBE_AMERICAS, "World");
+    RenderTabButton(HEAD_MISCELLANEOUS, ICON_FA_BOXES, "Miscellaneous");
+    RenderTabButton(HEAD_SETTINGS, ICON_FA_COG, "Settings");
+    RenderTabButton(HEAD_EXIT, ICON_FA_SIGN_OUT_ALT, "Exit");
+
+    ImGui::PopFont();
+
+    End();
+    PopStyleVar(2);
+    PopStyleColor(3);
 }
 
 inline bool CreateDeviceD3D(HWND hWnd) {
